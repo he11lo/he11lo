ddf773d719de72bf60a6cfb5adb6c8a574c50558
he11lo <136473078+he11lo@users.noreply.github.com>
2024-08-22 16:05:59 +0700

    Create my first project ever (can easily made by kid)

---
+++ my first project ever (can easily made by kid).cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello world. " << "It's my first project"; // easiest part //
+  cout << "Also, what is another library I can use cout func or anything like that?";
+}
